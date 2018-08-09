95af5dc9affbc8dc80c269f6e569c98979532fe9
vanyanosov <nosov582125@gmail.com>
2018-08-09 21:12:06 +0300

    new project

---
+++ barni/index_vanya.js
@@ -0,0 +1,46 @@
+const array = [2, 'sdfsdf', 22.1];
+
+// for (let i = 0; i < array.length; i++) {
+//     const variable = array[i];
+//     if (variable === '2') {
+//        console.log(1)
+//     }
+// };
+
+
+const obj = [{
+        name: 'vanya',
+        age: '14',
+        hobby: 'computer games'
+},
+{
+            name: 'ser',
+            age: '14',
+            hobby: 'computer games'
+},
+{
+            name: 'vasya',
+            age: '14',
+            hobby: 'computer games'
+},
+{
+            name: 'petya',
+            age: '14',
+            hobby: 'computer games'
+},
+{
+            name: 'yura',
+            age: '14',
+            hobby: 'computer games'
+},
+];
+
+for (let i = 0; i < obj.length; i++) {
+    console.log(obj[i].age)
+};
+
+const ofdf = [{
+     
+},
+
+]
---
+++ barni/webpack.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    module: {
+        rules: [{
+            test: /\.sass$/,
+            use: [{
+                loader: "style-loader"
+            }, {
+                loader: "css-loader"
+            }, {
+                loader: "sass-loader",
+                options: {
+                    includePaths: ["absolute/path/a", "absolute/path/b"]
+                }
+            }]
+        }]
+    }
+};
