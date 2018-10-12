538c2fa541e17d1cc46a98f143e7e6f7724538b1
vanyanosov <nosov582125@gmail.com>
2018-10-12 12:53:33 +0300

    timer

---
+++ timer/timer.js
@@ -0,0 +1,23 @@
+class Timer {
+    constructor(step) {
+      this.time = 0;
+      this.step = step;
+  }
+    timer() {
+      this.time = this.time + this.step;
+      return this.time;
+    }
+
+    stop() {
+      this.time = 0;
+      return this.time; 
+    }
+  }
+  
+  const time = new Timer(1);
+  
+  const interval = setInterval(() => {
+    const currentTime = time.timer();
+    console.log(currentTime)
+    if (currentTime === 0) clearInterval(interval);
+  }, 1000);
