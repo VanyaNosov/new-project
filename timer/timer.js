33eddabcf70599e0fb27537e95e5fc3813013ea1
vanyanosov <nosov582125@gmail.com>
2018-10-12 13:04:27 +0300

    new timer

--- timer/timer.js
+++ timer/timer.js
@@ -1,23 +1,23 @@
 class Timer {
-    constructor(step) {
-      this.time = 0;
-      this.step = step;
+  constructor(step) {
+    this.time = 0;
+    this.step = step;
+}
+  timer() {
+    this.time = this.time + this.step;
+    return this.time;
   }
-    timer() {
-      this.time = this.time + this.step;
-      return this.time;
-    }
 
-    stop() {
-      this.time = 0;
-      return this.time; 
-    }
+  stop() {
+    this.time = 0;
+    return this.time
   }
-  
-  const time = new Timer(1);
-  
-  const interval = setInterval(() => {
-    const currentTime = time.timer();
-    console.log(currentTime)
-    if (currentTime === 0) clearInterval(interval);
-  }, 1000);
+}
+
+const time = new Timer(1);
+
+const interval = setInterval(() => {
+  const currentTime = time.timer();
+  console.log(currentTime)
+  if (currentTime === 10) clearInterval(interval);
+}, 1000);
