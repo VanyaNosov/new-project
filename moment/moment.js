aebf3f1115e50171702998cd0a8efe76f5387f78
vanyanosov <nosov582125@gmail.com>
2018-12-20 18:25:24 +0200

    moment_library

---
+++ moment/moment.js
@@ -0,0 +1,6 @@
+console.log(moment().format('YYYY'));
+console.log(moment().format('YY'));
+console.log(moment().format('Q'));
+console.log(moment().format('X'));
+console.log(moment().format('x'));
+console.log(moment().format('DD.MM.YYYY'));
