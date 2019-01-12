51cdf57398e7cc5400d54cd2379bfe494f9db815
vanyanosov <nosov582125@gmail.com>
2019-01-13 00:00:24 +0200

    preloader

--- conversion/currency_conversion.js
+++ conversion/currency_conversion.js
@@ -1,5 +1,8 @@
+
+
 let dollarPrice = null;
 function arrayJson (json) {
+    console.log(json)
         for(var i = 0; i < json.length; i++){
             if(json[i].ccy === "USD"){
                 dollarPrice = json[i].buy;
@@ -10,13 +13,54 @@ function arrayJson (json) {
 function conversion () {
     let number = document.getElementById("Converter").value;
     let dollar_conversion = document.getElementById("convertion_dollar").value = innerHTML = parseInt(dollarPrice) * parseInt(number);
+
+    let sel = document.getElementById("menu");
+    let options = document.getElementById("menu_block").value;
+    console.log(options)
+    console.log(menu)
+    console.log(menu1)
+    console.log(menu2)
 };
+
+function funk () {
+    let loader = document.getElementById("page-preloader").style.visibility = 'hidden';
+}
+setTimeout(funk,3000);
+
+
 
 fetch('https://api.privatbank.ua/p24api/pubinfo?exchange&json&coursid=11')
     .then(res => res.json())
     .then(json => arrayJson(json));
 
 conversion();
+
+
+
+// let dollarPrice = null;
+// function arrayJson (json) {
+//         for(var i = 0; i < json.length; i++){
+//             if(json[i].ccy === "USD"){
+//                 dollarPrice = json[i].buy;
+//             };
+//         };
+// };
+
+// function conversion () {
+//     let number = document.getElementById("Converter").value;
+//     let dollar_conversion = document.getElementById("convertion_dollar").value = innerHTML = parseInt(dollarPrice) * parseInt(number);
+// };
+
+
+
+
+
+
+// fetch('https://api.privatbank.ua/p24api/pubinfo?exchange&json&coursid=11')
+//     .then(res => res.json())
+//     .then(json => arrayJson(json));
+
+// conversion();
 
 
 
@@ -39,7 +83,6 @@ conversion();
 
 // async function conversion (dollarPrice) { 
 //     const result = await functionSome();
-//     console.log(result);
 //     let convertion = document.getElementById("Converter");
 //     let dollar_conversion = document.getElementById("convertion_dollar").value = innerHTML = parseInt(dollarPrice) * parseInt(result);
 //         console.log(dollar_conversion)
